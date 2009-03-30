03b3c48a1c082dfd64f6352d4596e7a2808e9943
bgschaid <bgschaid@e4e07f05-0c2f-0410-a05a-b8ba57e0c909>
2009-03-30 08:53:53 +0000

    makeAxialMesh now also compiles under 1.5-dev with the helb of some #ifdefs
    git-svn-id: svn://svn.code.sf.net/p/openfoam-extend/svn/trunk/Breeder_1.5/utilities/mesh/manipulation/MakeAxialMesh@1132 e4e07f05-0c2f-0410-a05a-b8ba57e0c909

--- makeAxialMesh/makeAxialMesh.C
+++ makeAxialMesh/makeAxialMesh.C
@@ -50,7 +50,11 @@ Description
 
 #include "argList.H"
 #include "Time.H"
+#ifdef FOAM_DEV
+#include "repatch.H"
+#else
 #include "repatchPolyTopoChanger.H"
+#endif
 
 #include "plane.H"
 #include "line.H"
@@ -182,7 +186,11 @@ void changeCoordinates(
   else 		  Info << "Projecting nodes" << endl;
   const scalar angle=wedgeAngle/2;
 
+#ifdef FOAM_DEV
+  repatch topo(mesh);
+#else
   repatchPolyTopoChanger topo(mesh);
+#endif
 
   pointField oldPoints=mesh.points();                //DPS the old points will be rotated too
   pointField newPoints(oldPoints.size());
@@ -235,7 +243,11 @@ void changeCoordinates(
 // Split the wedge-patch into two patches
  
 void splitWedge(polyMesh &mesh,word wname,plane pl) {
+#ifdef FOAM_DEV
+  repatch topo(mesh);
+#else
   repatchPolyTopoChanger topo(mesh);
+#endif
 
   const polyBoundaryMesh& patches = mesh.boundaryMesh();
   const polyPatch &wedge=patches[patches.findPatchID(wname)];
@@ -358,13 +370,19 @@ void splitWedge(polyMesh &mesh,word wname,plane pl) {
 
   Info << " Changing patches\n" << endl;
 
+#ifdef FOAM_DEV
+  topo.execute();
+#else
   topo.repatch();
-
+#endif
 }
 
 void changeTypes(polyMesh &mesh,word wedge,word axis,bool hasOffset) {
+#ifdef FOAM_DEV
+  repatch topo(mesh);
+#else
   repatchPolyTopoChanger topo(mesh);
-
+#endif
   const polyBoundaryMesh& patches = mesh.boundaryMesh();
 
   List<polyPatch*> newPatches(patches.size());
